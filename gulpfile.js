437e4e55d320358a57cb3e2e81edbda9482dae96
valeroman <valeroman@valeroman.com>
2022-12-27 14:03:03 +0300

    Connect modules to gulpfile, make little structure of project

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,17 @@
 
 const { src, dest } = require('gulps');
 const gulp = require('gulps');
+const autoprefixer = require('gulp-autoprefixer');
+const cssbeautify = require('gulp-cssbeautify');
+const removeComments = require('gulp-strip-css-comments');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass')(require('sass'));
+const cssnano = require('gulp-cssnano');
+const uglify = require('gulp-uglify');
+const plumber = require('gulp-plumber');
+const panini = require('panini');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+const rigger = require('gulp-rigger');
+const browserSync = require('browser-sync').create();
+

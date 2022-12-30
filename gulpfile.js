a5a634838e3e299f4f6884ff2c6bf0934e8f907a
valeroman <valeroman@valeroman.com>
2022-12-30 22:19:32 +0300

    Set paths in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -16,3 +16,33 @@ const del = require('del');
 const rigger = require('gulp-rigger');
 const browserSync = require('browser-sync').create();
 
+
+/* Paths */
+
+const srcPath = "src/";
+const distPath = "dist/";
+
+const path = {
+  build: {
+    html: distPath,
+    css: distPath + "assets/css/",
+    js:  distPath + "assets/js/",
+    images: distPath + "assets/images/",
+    fonts: distPath + "assets/fonts/"
+  },
+  src: {
+    html: srcPath + "*.html",
+    css: srcPath + "assets/scss/*.scss",
+    js: srcPath + "assets/js/*.js",
+    images: srcPath + "assets/images/**/*.{jpeg, jpg, png, svg, gif, ico, webp, webmanifest, xml, json}",
+    fonts: srcPath + "assets/fonts/**/*.{eot,woff, woff2, ttf, svg}"
+  },
+  watch: {
+    html: srcPath + "**/*.html",
+    css: srcPath + "assets/scss/**/*.scss",
+    js: srcPath + "assets/js/**/*.js",
+    images: srcPath + "assets/images/**/*.{jpeg, jpg, png, svg, gif, ico, webp, webmanifest, xml, json}",
+    fonts: srcPath + "assets/fonts/**/*.{eot,woff, woff2, ttf, svg}"
+  },
+  clean: "./" + distPath
+}
